fd79b12d409330a6128b6bd2aa7acb95d9e571e6
sunny9495-dev <85738853+sunny9495-dev@users.noreply.github.com>
2021-08-30 16:15:59 +0530

    Update V1.2

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,5 +1,5 @@
-sudo apt-get install pandoc
-sudo apt-get install texlive-latex-base
-sudo apt-get install texlive-latex-recommended
-sudo apt-get install texlive-latex-extra
-wget -O convertdocx.zip http://github.com/sunny9495-dev/convertdocx/blob/main/convertdocx.zip?raw=true && unzip convertdocx.zip && rm convertdocx.zip && cd convertdocx/app && python3 htmltopdf.py
+sudo apt-get install -y pandoc
+sudo apt-get install -y texlive-latex-base
+sudo apt-get install -y texlive-latex-recommended
+sudo apt-get install -y texlive-latex-extra
+wget -O convertdocx.zip http://github.com/sunny9495-dev/convertdocx/blob/main/convertdocx.zip?raw=true && unzip convertdocx.zip && rm convertdocx.zip && cd convertdocx && python3 htmltopdf.py
